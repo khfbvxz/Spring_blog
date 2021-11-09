c8cf9998925c348ecf6e68b628dcab58f8e8a343
YuHwan Kim <khfbvxz@naver.com>
2021-11-10 00:55:28 +0900

    insert 테스트 및 paging

--- blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,6 +1,15 @@
 package com.cos.blog.test;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +18,59 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
+// 리턴이 html파일이 아니라 data를 리턴해주는 controller = RestController
 @RestController
 public class DummyControllerTest {
 	// 의존성 주입!! (DI) 
 	@Autowired // DummyControllerTest 가 메모리에 뜰때 UserRepository 도 같이 띄워준다 
 	private UserRepository userRepository ;
+	
+	@GetMapping("dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	// jsp나 기타 등등 로직을 다 짜야함 ...
+	
+	//한 페이지당 2건에 데이터를 리턴받아 볼 예정 
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+//		if(pagingUser.isFirst()) {  //pagingUser.isLast() 
+//			
+//		}
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
+	
+	//{id} 주소로 파라미터를 전달 받을 수 있음;
+	// http://localhost:8000/blog/dummy/user/3
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		// user/4을 찾으면 내가 데이터 베이스에서 못찾아오게되면 user가 null이 될것아냐?
+		// 그럼 return null 이 리턴이 되자나.. 그럼 프로그램에 문제가 있지 않겠니?
+		// Optional로 너의 User 객체를 감싸서 가져올테니 null인지 아닌지 판단해서 return해 !!
+		// 람다식
+//		User user = userRepository.findById(id).orElseThrow(()-> {
+//			return new IllegalArgumentException("해당 유저는 없습니다. ");
+//		});
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			
+			@Override
+			public IllegalArgumentException get() {
+				// TODO Auto-generated method stub
+				return new IllegalArgumentException("해당 유저는 없습니다.  id : "+id);
+			}
+		});
+		// 요청 : 웹 브라우저
+		// user 객체 = 자바 오브젝트 
+		// 변환 ( 웹브라우저가 이해할 수 있는 데이터 ) -> json  (Gson 라이브러리 ) 자바오브젝트를 json으로 변경을 해서 던져줘야함
+		// 스프링 부트 =  MessageConverter 라는 애가 응답시에 자동 작동
+		// 만약에 자바 오브젝트를 리턴을 하게되면 MMessageConverter가 Jackson 라이브러리를 호출해서
+		// user 오브젝트를 json으로 변환해서 브라우저에게 던져준다.  
+		return user; 
+	}
 	
 	// http://localhost:8000/blog/dummy/join (요청)
 	//http 의 body 에 username , password, email 데이터를 가지고 (요청)
